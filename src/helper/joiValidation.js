35f70627477bd7679b35958ef892360dc92464e2
Maggi <vadivelleninkumar@gmail.com>
2022-06-20 20:13:11 +0530

    validation added for signin

--- src/helper/joiValidation.js
+++ src/helper/joiValidation.js
@@ -10,3 +10,11 @@ exports.signupSchema = (req) => {
     })
     return schema.validate(req, { abortEarly: false });
 }
+
+exports.signupSchema = (req) => {
+    let schema = Joi.object({
+        emailId: Joi.string().email(({ minDomainSegments: 2, tlds: { allow: ['com', 'io'] } })).lowercase().required(),
+        password: Joi.string().alphanum().min(8).max(15).required()
+    })
+    return schema.validate(req, { abortEarly: false });
+}
